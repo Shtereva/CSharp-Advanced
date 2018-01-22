662be222ecb9c10268fe2ae710e8b90a6c846925
Shtereva <a.i.shtereva@gmail.com>
2018-01-22 20:22:29 +0200

    Stacks and Queues

---
+++ Stacks and Queues/06. Traffic Jam/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Traffic_Jam
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int passing = int.Parse(Console.ReadLine());
+
+            string cars = string.Empty;
+            var traffic = new Queue<string>();
+            int passedCars = 0;
+
+            while ((cars = Console.ReadLine()) != "end")
+            {
+                if (cars == "green")
+                {
+                    for (int i = 0; i < passing; i++)
+                    {
+                        if (traffic.Count > 0)
+                        {
+                            Console.WriteLine($"{traffic.Peek()} passed!");
+                            traffic.Dequeue();
+                            passedCars++;
+                        }
+                    }
+
+                    continue;
+                }
+                traffic.Enqueue(cars);
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
